3fc16fb45fee43300fd6fe4fc84227fbf69b4832
jydeng <529909869@qq.com>
2019-04-25 22:44:01 +0800

    首页背景视频（50%）

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,30 +1,37 @@
 <template>
-  <el-row class="wrapper">
-    <el-col :md="12" class="left">
-      <div class="logo"></div>
-    </el-col>
-    <el-col :md="12" class="right">
-      <div class="user">
-        <el-input
-          placeholder="请输入你的账号"
-          clearable
-          prefix-icon="fa fa-fw fa-user"
-          v-model="user.username"
-        ></el-input>
-        <el-input
-          placeholder="请输入你的密码"
-          type="password"
-          prefix-icon="fa fa-fw fa-key"
-          clearable
-          v-model="user.password"
-          @keyup.enter.native="handleLogin"
-        ></el-input>
-        <el-button type="primary" @click="handleLogin">
-          <i class="fa fa-fw fa-power-off"></i> 登 录
-        </el-button>
-      </div>
-    </el-col>
-  </el-row>
+  <div class="wrapper">
+    <div id="overlay"></div>
+    <video autoplay loop poster="polina.jpg" id="bgVideo">
+      <source src="video/night.webm" type="video/webm" />
+      <source src="video/night.mp4" type="video/mp4" />
+    </video>
+    <el-row class="loginPane">
+      <el-col :md="12" class="left">
+        <div class="logo"></div>
+      </el-col>
+      <el-col :md="12" class="right">
+        <div class="user">
+          <el-input
+            placeholder="请输入你的账号"
+            clearable
+            prefix-icon="fa fa-fw fa-user"
+            v-model="user.username"
+          ></el-input>
+          <el-input
+            placeholder="请输入你的密码"
+            type="password"
+            prefix-icon="fa fa-fw fa-key"
+            clearable
+            v-model="user.password"
+            @keyup.enter.native="handleLogin"
+          ></el-input>
+          <el-button type="primary" @click="handleLogin">
+            <i class="fa fa-fw fa-power-off"></i> 登 录
+          </el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
 import { mapActions } from "vuex";
@@ -86,34 +93,57 @@ export default {
 <style lang="scss" scoped>
 @import "../style/mixin";
 .wrapper {
-  width: 600px;
-  @include center;
-  @include card;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(~/video/video_cover.jpeg);
 
-  .left {
-    height: 260px;
-    background-image: url(../assets/logo.png);
-    background-position: center center;
-    background-size: 80px;
-    background-repeat: no-repeat;
+  #bgVideo {
+    @include center;
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
   }
 
-  .right {
-    position: relative;
-    height: 260px;
+  #overlay {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
 
-    .user {
-      width: 100%;
-      box-sizing: border-box;
-      padding: 0 10px;
-      @include center;
+  .loginPane {
+    width: 600px;
+    @include center;
+    @include card;
 
-      .el-input {
-        line-height: 55px;
-      }
-      .el-button {
+    .left {
+      height: 260px;
+      background-image: url(../assets/logo.png);
+      background-position: center center;
+      background-size: 80px;
+      background-repeat: no-repeat;
+    }
+
+    .right {
+      position: relative;
+      height: 260px;
+
+      .user {
         width: 100%;
-        margin-top: 10px;
+        box-sizing: border-box;
+        padding: 0 10px;
+        @include center;
+
+        .el-input {
+          line-height: 55px;
+        }
+        .el-button {
+          width: 100%;
+          margin-top: 10px;
+        }
       }
     }
   }
@@ -121,13 +151,15 @@ export default {
 
 @media screen and (max-width: 768px) {
   .wrapper {
-    width: 300px;
-    .left {
-      height: 160px;
-      border-right: none;
-    }
-    .right {
-      height: 180px;
+    .loginPane {
+      width: 300px;
+      .left {
+        height: 160px;
+        border-right: none;
+      }
+      .right {
+        height: 180px;
+      }
     }
   }
 }
