c299b2a71b7a9af968f6044f7c98bdadd8174613
jydeng <529909869@qq.com>
2019-05-18 22:07:12 +0800

    zrender实践，实现一个消息循环

--- src/views/home/other/index.vue
+++ src/views/home/other/index.vue
@@ -31,11 +31,20 @@
         ></iUploadImg>
       </el-col>
     </el-row>
+    <el-row class="item">
+      <el-col :md="24" :sm="24">
+        <p class="desc">5.通知循环滚动：</p>
+        <div class="list" ref="list">
+          <!-- <div v-for="item in list" :key="item.id">{{item.txt}}</div> -->
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
 import ElementParamsInput from "element-params-input";
 import Clipboard from "clipboard";
+import zrender from "zrender";
 export default {
   name: "other",
   components: {
@@ -46,8 +55,55 @@ export default {
       test: "https://www.a.com?a=1&b=2&c=3&d=4",
       txt: "随便写点东西",
       clipboard: null,
-      img: ""
+      img: "",
+      list: [
+        { id: 1, txt: "测试列表一" },
+        { id: 2, txt: "测试列表二" },
+        { id: 3, txt: "测试列表三" },
+        { id: 4, txt: "测试列表四" },
+        { id: 5, txt: "测试列表五" },
+        { id: 6, txt: "测试列表六" },
+        { id: 7, txt: "测试列表七" },
+        { id: 8, txt: "测试列表八" },
+        { id: 9, txt: "测试列表九" },
+        { id: 10, txt: "测试列表十" }
+      ],
+      timer: null
     };
+  },
+  methods: {
+    render(zr, zrender) {
+      let w = zr.getWidth();
+      let list = [];
+      for (let index = 0; index < this.list.length; index++) {
+        const item = this.list[index];
+        const node = new zrender.Text({
+          style: {
+            text: item.txt,
+            textAlign: "center",
+            textVerticalAlign: "middle",
+            fontSize: 14,
+            fontFamily: "Lato",
+            textFill: "#000",
+            blend: "lighten"
+          },
+          position: [w / 2, 15 * index + 10]
+        });
+
+        list.push(node);
+        zr.add(node);
+      }
+      this.timer = setInterval(() => {
+        for (let index = 0; index < list.length; index++) {
+          const element = list[index];
+          if (element.position[1] < 0) {
+            element.attr("position", [w / 2, 148]);
+          } else {
+            element.attr("position", [w / 2, element.position[1] - 1]);
+          }
+        }
+      }, 60);
+    }
   },
   mounted() {
     this.clipboard = new Clipboard(".cp", {
@@ -61,10 +117,16 @@ export default {
         return this.txt;
       }
     });
+
+    let zr = zrender.init(this.$refs["list"]);
+    this.render(zr, zrender);
   },
   beforeDestroy() {
     if (this.clipboard) {
       this.clipboard.destroy();
+    }
+    if (this.timer) {
+      window.clearInterval(this.timer);
     }
   }
 };
@@ -77,6 +139,14 @@ export default {
 .desc {
   font-size: 14px;
   color: #010101;
-  margin: 5px 0;
+}
+.list {
+  height: 100px;
+  width: 100px;
+  overflow-y: hidden;
+  div {
+    font-size: 13px;
+    margin: 5px 0;
+  }
 }
 </style>
